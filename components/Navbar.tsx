20fcf8dd20dd9366f0bcf0228972c9e10b133eb1
William Iadarola <wiadarola0@gmail.com>
2023-09-22 15:40:46 -0700

    Sidebar & mobile sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,14 +7,15 @@ import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./theme-toggle";
+import MobileSidebar from "@/components/MobileSidebar";
 
 const font = Poppins({ weight: "600", subsets: ["latin"] });
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary  h-16">
             <div className="flex items-center">
-                < Menu className="block md:hidden" />
+                <MobileSidebar />
                 < Link href="/" />
                 <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
                     companion.ai
