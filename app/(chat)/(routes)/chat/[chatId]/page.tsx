4ab7f7bb7c6267f95af1d3c2d7f629855326eeef
William Iadarola <wiadarola0@gmail.com>
2023-09-23 08:43:01 -0700

    Add query for companion history per user

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,11 +1,50 @@
-interface ChatIdPageProps {
+import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import ChatClient from "./components/ChatClient";
 
+interface ChatIdPageProps {
+    params: {
+        chatId: string;
+    }
 }
 
-const ChatIdPage: React.FunctionComponent<ChatIdPageProps> = () => {
+const ChatIdPage: React.FunctionComponent<ChatIdPageProps> = async ({ params }) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        // Disabled, as I want non-signed in users to be able to view the chat
+        // return redirectToSignIn();
+    }
+
+    const companion = await prismadb.companion.findUnique({
+        where: {
+            id: params.chatId
+        },
+        include: {
+            messages: {
+                orderBy: {
+                    createdAt: "asc"
+                },
+                where: {
+                    userId: userId || undefined,
+                }
+            },
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    });
+
+    if (!companion) {
+        return redirect("/")
+    }
+
     return (
         <div>
-            ChatIdPage
+            <ChatClient companion={companion} />
         </div>
     );
 }
