af6e832631f651a1866325a11e5882df6f0f9368
William Iadarola <wiadarola0@gmail.com>
2023-09-23 11:14:27 -0700

    Chat form and front end process for sending messages

--- app/(chat)/(routes)/chat/[chatId]/components/ChatClient.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/ChatClient.tsx
@@ -2,6 +2,10 @@
 
 import { Companion, Message } from "@prisma/client";
 import ChatHeader from "./ChatHeader";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useCompletion } from "ai/react";
+import ChatForm from "./ChatForm";
 
 interface ChatClientProps {
     companion: Companion & {
@@ -13,9 +17,39 @@ interface ChatClientProps {
 }
 
 const ChatClient: React.FunctionComponent<ChatClientProps> = ({ companion }) => {
+    const router = useRouter();
+    const [messages, setMessages] = useState<any[]>(companion.messages);
+
+    const { input, isLoading, handleInputChange, handleSubmit, setInput } = useCompletion({
+        api: `/api/chat/${companion.id}`,
+        onFinish(prompt, completion) {
+            const systemMessage = {
+                role: "system",
+                content: completion,
+            }
+
+            setMessages(prev => [...prev, systemMessage]);
+            setInput("");
+
+            router.refresh();
+        },
+    });
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        const userMessage = {
+            role: "user",
+            content: input,
+        }
+
+        setMessages(prev => [...prev, userMessage]);
+
+        handleSubmit(e);
+    };
+
     return (
         <div className="flex flex-col h-full p-4 space-y-2">
             <ChatHeader companion={companion} />
+            <ChatForm isLoading={isLoading} input={input} handleInputChange={handleInputChange} onSubmit={onSubmit} />
         </div>
     );
 }
---
+++ app/(chat)/(routes)/chat/[chatId]/components/ChatForm.tsx
@@ -0,0 +1,24 @@
+import { ChatRequestOptions } from "ai";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SendHorizonal } from "lucide-react";
+
+interface ChatFormProps {
+    isLoading: boolean;
+    input: string;
+    handleInputChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+    onSubmit: (e: React.FormEvent<HTMLFormElement>, chatRequestOptions?: ChatRequestOptions | undefined) => void;
+}
+
+const ChatForm: React.FunctionComponent<ChatFormProps> = ({ isLoading, input, handleInputChange, onSubmit }) => {
+    return (
+        <form onSubmit={onSubmit} className="border-t border-primary/10 py-4 flex items-center gap-x-2">
+            < Input disabled={isLoading} value={input} onChange={handleInputChange} placeholder="Type a message" className="rounded-lg bg-primary/10" />
+            < Button disabled={isLoading} variant={'ghost'}>
+                <SendHorizonal className="h-6 w-6" />
+            </Button>
+        </form>
+    );
+}
+
+export default ChatForm;
