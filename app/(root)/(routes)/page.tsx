c51b83f6eef83102ccd182a63dc19a1be91ebe17
William Iadarola <wiadarola0@gmail.com>
2023-09-22 18:15:10 -0700

    Add shadcn packages

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,14 @@
 import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
+import Categories from "@/components/Categories";
 
-const RootPage = () => {
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany();
+
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories} />
         </div>
     );
 }
