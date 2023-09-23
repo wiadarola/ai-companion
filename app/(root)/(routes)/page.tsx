f9d7671353b5de9072bf3b744a7e045d05b9552f
William Iadarola <wiadarola0@gmail.com>
2023-09-22 21:42:31 -0700

    Companion cards

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,45 @@
 import SearchInput from "@/components/SearchInput";
 import prismadb from "@/lib/prismadb";
 import Categories from "@/components/Categories";
+import { currentUser } from "@clerk/nextjs";
+import Companions from "@/components/Companions";
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage: React.FC<RootPageProps> = async ({ searchParams }) => {
+    const user = await currentUser();
+
+    const data = await prismadb.companion.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name,
+            },
+            userId: "user_2VlkYg7lPaUBaG0ZxFwcv6orhuL" || user?.id,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true,
+                }
+            }
+        }
+    })
     const categories = await prismadb.category.findMany();
 
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
     );
 }
